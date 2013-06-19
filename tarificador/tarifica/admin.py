0bb6584535dd72a176dec9bee210a0d413291346
Luis Lizarraga <siul.al4@gmail.com>
2013-06-19 15:28:06 -0700

    Creado los modelos

--- tarificador/tarifica/admin.py
+++ tarificador/tarifica/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from tarifica.models import Provider
+from tarifica.models import Provider, Destination, Plan, FreeNumberPlan
 
 admin.site.register(Provider)
+admin.site.register(Destination)
+admin.site.register(Plan)
+admin.site.register(FreeNumberPlan)
