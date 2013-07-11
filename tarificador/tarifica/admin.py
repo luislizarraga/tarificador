08e96d3e6a3c50ff09ff6811663a9bb884fa8678
Luis Lizarraga <siul.al4@gmail.com>
2013-07-11 17:20:59 -0500

    Creado los modelos y los formularios, fase de prueba

--- tarificador/tarifica/admin.py
+++ tarificador/tarifica/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from tarifica.models import Provider
+from tarifica.models import Provider, PaymentType
 
 admin.site.register(Provider)
+admin.site.register(PaymentType)
