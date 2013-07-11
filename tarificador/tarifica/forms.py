87114f22f230cfa583b7b4b370bcbd2b25e7eb23
Luis Lizarraga <siul.al4@gmail.com>
2013-07-11 16:39:31 -0500

    Creado los modelos y los formularios, fase de prueba

--- tarificador/tarifica/forms.py
+++ tarificador/tarifica/forms.py
@@ -6,7 +6,7 @@ class AddProviderInfo(forms.Form):
     name = forms.CharField(label = 'Nombre')
     monthly_cost = forms.FloatField(label = 'Renta Mensual')
     period_end = forms.DateTimeField(label = 'Fecha de Corte')
-    payment_type = forms.ChoiceField(choices = [(e, e.name) for e in PaymentType.objects.all()], label = 'Modalidad de Pago')
+    #payment_type = forms.ChoiceField(choices = [(e, e.name) for e in PaymentType.objects.all()], label = 'Modalidad de Pago')
     channels = forms.IntegerField(label = 'Canales')
 
 
@@ -14,14 +14,14 @@ class AddBaseTariffs(forms.Form):
     destination_group = forms.CharField(max_length = 255, label = 'Localidad')
     prefix = forms.CharField(max_length = 255, label = 'Prefijo')
     matching_number = forms.CharField(max_length = 255, label = 'Expresion Regular')
-    tariff_mode = forms.ChoiceField(choices = [(e, e.name) for e in TariffMode.objects.all()], label = 'Modo')
+    #tariff_mode = forms.ChoiceField(choices = [(e, e.name) for e in TariffMode.objects.all()], label = 'Modo')
     cost = forms.FloatField(label = 'Costo')
 
 
 class AddBundles(forms.Form):
     name = forms.CharField(max_length = 255, label = 'Nombre de Paquete')
-    destination_group = forms.ChoiceField(choices = [(e, e.name) for e in DestinationGroup.objects.all()], label = 'Localidad')
-    tariff_mode = forms.ChoiceField(choices = [(e, e.name) for e in TariffMode.objects.all()], label = 'Modo')
+    #destination_group = forms.ChoiceField(choices = [(e, e.name) for e in DestinationGroup.objects.all()], label = 'Localidad')
+    #tariff_mode = forms.ChoiceField(choices = [(e, e.name) for e in TariffMode.objects.all()], label = 'Modo')
     cost = forms.FloatField(label = 'Costo')
 
 
