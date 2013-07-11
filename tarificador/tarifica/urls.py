238ba6c27b73d76ce5b8ee19de5b571a1ef391d3
Luis Lizarraga <siul.al4@gmail.com>
2013-07-11 16:50:13 -0500

    Creado los modelos y los formularios, fase de prueba

--- tarificador/tarifica/urls.py
+++ tarificador/tarifica/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from tarifica import views
 
 urlpatterns = patterns('',
-                       url(r'setuptroncales', views.setupUno, name = 'setup'),
+                       url(r'setuptroncales$', views.setupUno, name = 'setup'),
+                       url(r'thanks$', views.thanks, name = 'gracias')
+                       url(r'contact$',views.thanks, name = 'contacto')
 )
