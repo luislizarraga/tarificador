4f9bd679cf1ef68e1c96781b2a60302a1568b12e
Luis Lizarraga <siul.al4@gmail.com>
2013-07-11 17:11:22 -0500

    Creado los modelos y los formularios, fase de prueba

--- tarificador/tarifica/views.py
+++ tarificador/tarifica/views.py
@@ -9,9 +9,9 @@ def setupUno(request):
     if request.method == 'POST': # If the form has been submitted...
         form = AddProviderInfo(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
-            lala = form.cleaned_data['provider']
+            lala = form.cleaned_data['name']
             # ...
-            return HttpResponseRedirect('tarifica/thanks', lala) # Redirect after POST
+            return HttpResponseRedirect('tarifica/thanks', name) # Redirect after POST
     else:
         form = AddProviderInfo() # An unbound form
 
