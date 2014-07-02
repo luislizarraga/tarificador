5d0f2ce05f387d1b4ede6e5db5781fa9964ac86e
Luis Lizarraga <siul.al4@gmail.com>
2014-07-01 23:04:00 -0500

    cambiando contra

--- tarificador/settings.py
+++ tarificador/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'tarificador',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'luis',
-        'PASSWORD': 'siulims',
+        'PASSWORD': 'weeee',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '81',                      # Set to empty string for default.
     }
